8699f4074a0187eb2c014aadc240a22f65e73f7d
Stephen Bayne <stephen.bayne@ncr.com>
2017-02-08 17:34:49 +0000

    Initial commit

---
+++ CefSharp.MinimalExample.WinForms/Test.cs
@@ -0,0 +1,25 @@
+﻿using CefSharp.WinForms;
+using System.Threading.Tasks;
+
+namespace CefSharp.MinimalExample.WinForms
+{
+    class Test
+    {
+        private readonly ChromiumWebBrowser _browser;
+
+        public Test(ChromiumWebBrowser browser)
+        {
+            _browser = browser;
+        }
+
+        public object testEval()
+        {
+            return _testEval().Result.Result;
+        }
+
+        private async Task<JavascriptResponse> _testEval()
+        {
+            return await _browser.EvaluateScriptAsync("eval", "'hello'");
+        }
+    }
+}
